58ab8cf47df4cf548d666a0b346e540d66dd310d
FireW2016 <86747501+FireW2016@users.noreply.github.com>
2021-06-30 23:50:20 +0200

    Update CBROFarm.lua

--- CBROFarm.lua
+++ CBROFarm.lua
@@ -1,13 +1,13 @@
 -- AutoFarm CBRO Fatto da Firo ( Non bypassa il limite di Players per ogni team) combinate con AntiAfk così non venite kickati
-
-
+ 
+ 
 if game.PointsService:FindFirstChild("Switch") then
-
+ 
 Switch = game.PointsService.Switch
 Executed = game.PointsService.Executed
-
+ 
 else
-
+ 
 Switch = Instance.new("BoolValue") -- EXECUTE 1 TIME
 Switch.Name = "Switch"                   -- ESEGUIRE 1 VOLTA
 Switch.Parent = game.PointsService
@@ -23,53 +23,53 @@ end
 if Executed.Value == false then
 Executed.Value = true
 while wait(10) do
-
-local CTScore = game.Players.WaterWarrior20161.PlayerGui.GUI.UpperInfo.CTScore.Text -- Mettete il nome roblox al posto di STOCAZZO per tutte
-local TScore = game.Players.WaterWarrior20161.PlayerGui.GUI.UpperInfo.TScore.Text  -- Put your roblox name instead of STOCAZZO for all
-local CurrentTeam = game.Players.WaterWarrior20161.Status.Team.Value
-
+ 
+local CTScore = game.Players.LocalPlayer.PlayerGui.GUI.UpperInfo.CTScore.Text 
+local TScore = game.Players.LocalPlayer.PlayerGui.GUI.UpperInfo.TScore.Text  
+local CurrentTeam = game.Players.LocalPlayer.Status.Team.Value
+ 
 if Switch.Value == true then
-
+ 
 if CTScore > TScore and CurrentTeam == "CT" then
-    
-    
+ 
+ 
 elseif CTScore > TScore and CurrentTeam == "T" then
-    
+ 
     local args = {
         [1] = "CT"
     }
-
+ 
     game:GetService("ReplicatedStorage").Events.JoinTeam:FireServer(unpack(args))
-    
+ 
 elseif TScore > CTScore and CurrentTeam == "T" then
-    
-    
+ 
+ 
 elseif TScore > CTScore and CurrentTeam == "CT" then
-    
-
+ 
+ 
     local args = {
         [1] = "T"
     }
-
+ 
         game:GetService("ReplicatedStorage").Events.JoinTeam:FireServer(unpack(args))
-        
+ 
 elseif CurrentTeam == "Spectator" then                                                                                                                       -- Fatto da Firo
  
     local args = {
         [1] = "T"
     }
-
+ 
         game:GetService("ReplicatedStorage").Events.JoinTeam:FireServer(unpack(args))
-
+ 
 wait(5)
     local args = {
         [1] = "CT"
     }
-
+ 
         game:GetService("ReplicatedStorage").Events.JoinTeam:FireServer(unpack(args))
-
+ 
 elseif TScore == CTScore then
-
+ 
 end
 end
 end
